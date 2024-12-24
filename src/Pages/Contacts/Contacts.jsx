427ac6b0b4dd885b390bcd2a51890d36f9ad8752
deleiscorrea <deleiscorrea13@gmail.com>
2024-12-24 12:12:54 -0300

    contacts

--- src/Pages/Contacts/Contacts.jsx
+++ src/Pages/Contacts/Contacts.jsx
@@ -34,7 +34,7 @@ const Contacts = () => {
             </div>
             <div style={{ display: "flex", flexDirection: "column", alignItems: "center", height: "100vh" }}>
                 <h1 style={{color: "white", fontSize: "40px", marginBottom: "20px", marginTop: "20px"}}>Contactos</h1>
-                <Link to='/contacts/new'><button style={{backgroundColor: "#F9E400", width: "60%", padding: "10px 0px 10px 0px"}}>Nuevo contacto</button></Link>
+                <Link to='/contacts/new'><button style={{backgroundColor: "#F9E400", width: "60%", padding: "10px 0px 10px 0px", display: "flex", alignItems: "center", justifyContent: "center"}}>Nuevo contacto</button></Link>
                 {
                     isLoadingContacts
                         ? <span>Cargando...</span>
@@ -60,7 +60,7 @@ const ContactsList = ({ contacts, onDeleteContact }) => {
 const Contact = ({ name, email, info, image, _id, onDeleteContact }) => {
     return (
         <div>
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <div style={{ display: "flex", alignItems: "center" }}>
                 <h1 style={{ display: "inline", color: "white" }}>{name}</h1>
                 <span className=''>{` (${_id})`}</span>
                 <br />
